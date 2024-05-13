e517ad9f7f919dd593d8c5c870a416b45fe721aa
sbj314@sympatico.ca <sbj314@sympatico.ca>
2024-05-13 14:19:06 +0000

    Created Pizza_Oven.RowTemplate1, ServerModule1, Pizza_Oven.ItemTemplate1 and Pizza_Oven; Edited settings; Deleted Form1

--- client_code/Pizza_Oven/__init__.py
+++ client_code/Pizza_Oven/__init__.py
@@ -1,7 +1,11 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Pizza_OvenTemplate
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Pizza_Oven(Pizza_OvenTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
