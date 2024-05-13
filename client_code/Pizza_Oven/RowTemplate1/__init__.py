efc74d425d6c23ead0e67884d395cd3d8735ae72
sbj314@sympatico.ca <sbj314@sympatico.ca>
2024-05-13 16:01:21 +0000

    Edited Pizza_Oven.RowTemplate1 and Pizza_Oven

--- client_code/Pizza_Oven/RowTemplate1/__init__.py
+++ client_code/Pizza_Oven/RowTemplate1/__init__.py
@@ -9,5 +9,30 @@ class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.countdown=0    # baking timer
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the BAKE button is clicked"""
+    self.item.update(status='Baking')
+    self.refresh_data_bindings()
+    self.timer_1.interval=30
+    self.countdown = 2
+    pass
+
+  def button_2_click(self, **event_args):
+    """This method is called when the BOX button is clicked"""
+    self.item.update(status='Boxing')
+    self.refresh_data_bindings()
+    pass
+
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    self.countdown -= 1
+    if self.countdown == 0:
+      self.row_color = Red
+    print (self.timer_1)
+    pass
+
+
+
